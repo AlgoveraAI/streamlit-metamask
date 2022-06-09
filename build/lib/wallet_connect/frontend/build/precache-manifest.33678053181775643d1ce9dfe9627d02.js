5a26956fb41747c4f1484ad3a8f2105506bd25c4
Jakub Smékal <jakub.smekal@gmail.com>
2022-06-09 10:55:43 +0200

    DONE: build version

---
+++ build/lib/wallet_connect/frontend/build/precache-manifest.33678053181775643d1ce9dfe9627d02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42c73ad85030ee36662c6a0e16fe410",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ae6a3d6b083469def7e",
+    "url": "./static/js/2.279307cf.chunk.js"
+  },
+  {
+    "revision": "8153f0a66a96426c080d6e63d8378b2a",
+    "url": "./static/js/2.279307cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4656b3e8122d37d807",
+    "url": "./static/js/main.069cae75.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.069cae75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675ebed85599bcec64c0",
+    "url": "./static/js/runtime-main.57a1a595.js"
+  }
+]);
