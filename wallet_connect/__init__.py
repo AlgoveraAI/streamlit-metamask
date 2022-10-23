d22ed769dec7a1f6c53612b8440eb9c7c1121bed
Jakub Smékal <jakub.smekal@gmail.com>
2022-10-23 15:07:30 +0100

    WIP: prepared encrypt & decrypt buttons in __init__.py

--- wallet_connect/__init__.py
+++ wallet_connect/__init__.py
@@ -3,16 +3,18 @@ import streamlit as st
 import os
 
 
-parent_dir = os.path.dirname(os.path.abspath(__file__))
-build_dir = os.path.join(parent_dir, "frontend/build")
-_wallet_connect = components.declare_component("wallet_connect", path=build_dir)
-# _wallet_connect = components.declare_component("wallet_connect", url="http://localhost:3001")
+# parent_dir = os.path.dirname(os.path.abspath(__file__))
+# build_dir = os.path.join(parent_dir, "frontend/build")
+# _wallet_connect = components.declare_component("wallet_connect", path=build_dir)
+_wallet_connect = components.declare_component("wallet_connect", url="http://localhost:3001")
 
 # default contract address is for ocean token
 def wallet_connect(label, key=None, message="Connect Wallet", contract_address="0xCfDdA22C9837aE76E0faA845354f33C62E03653a",amount="0.01", to_address=""):
     return _wallet_connect(label=label, default="not", key=key, message=message, contract_address=contract_address, amount=amount, to_address=to_address)
 
 
-# wallet_button = wallet_connect(label="wallet", key="wallet")
-# st.write(f"Wallet {wallet_button} connected.")
+wallet_button = wallet_connect(label="wallet", key="wallet")
+st.write(f"Wallet {wallet_button} connected.")
 # second_button = wallet_connect(label="send", key="send", message="Send Transaction", contract_address="0xCfDdA22C9837aE76E0faA845354f33C62E03653a", amount="10", to_address="") # need to fill in to_address
+encrypt_button = wallet_connect(label="encrypt", key="encrypt")
+decrypt_button = wallet_connect(label="decrypt", key="decrypt")
