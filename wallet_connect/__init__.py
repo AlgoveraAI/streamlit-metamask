ecdc29696d04745bea2307a6285217df93565364
Jakub Smékal <jakub.smekal@gmail.com>
2022-10-30 14:04:52 +0000

    WIP: removed session_state

--- wallet_connect/__init__.py
+++ wallet_connect/__init__.py
@@ -9,12 +9,20 @@ import os
 _wallet_connect = components.declare_component("wallet_connect", url="http://localhost:3001")
 
 # default contract address is for ocean token
-def wallet_connect(label, key=None, message="Connect Wallet", contract_address="0xCfDdA22C9837aE76E0faA845354f33C62E03653a",amount="0.01", to_address=""):
-    return _wallet_connect(label=label, default="not", key=key, message=message, contract_address=contract_address, amount=amount, to_address=to_address)
+def wallet_connect(label, key=None, message="Connect Wallet", contract_address="0xCfDdA22C9837aE76E0faA845354f33C62E03653a",amount="0.01", to_address="", message_to_encrypt="", encrypted_string="", encrypted_symmetric_key=""):
+    return _wallet_connect(label=label, default="not", key=key, message=message, contract_address=contract_address, amount=amount, to_address=to_address, message_to_encrypt=message_to_encrypt, encrypted_string=encrypted_string, encrypted_symmetric_key=encrypted_symmetric_key)
 
 
 wallet_button = wallet_connect(label="wallet", key="wallet")
 st.write(f"Wallet {wallet_button} connected.")
 # second_button = wallet_connect(label="send", key="send", message="Send Transaction", contract_address="0xCfDdA22C9837aE76E0faA845354f33C62E03653a", amount="10", to_address="") # need to fill in to_address
-encrypt_button = wallet_connect(label="encrypt", key="encrypt")
-decrypt_button = wallet_connect(label="decrypt", key="decrypt")
+message_to_encrypt = st.text_input("Message to encrypt")
+encrypt_button = wallet_connect(label="encrypt", key="encrypt", message="Encrypt", message_to_encrypt=message_to_encrypt)
+st.write(encrypt_button)
+
+# st.session_state["encrypted_string"] = encrypt_button["encryptedString"]
+# st.session_state["encrypted_symmetric_key"] = encrypt_button["encryptedSymmetricKey"]
+encrypted_string = st.text_input("Enter string to decrypt")
+encrypted_symmetric_key = st.text_input("Enter symmetric key")
+
+decrypt_button = wallet_connect(label="decrypt", key="decrypt", message="Decrypt", encrypted_string=encrypted_string, encrypted_symmetric_key=encrypted_symmetric_key)
